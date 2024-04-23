84b3cbb30aecf1a8fea28a56be33dde500d1df14
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-04-23 22:09:45 +0800

    Add files via upload

---
+++ generation.py
@@ -0,0 +1,141 @@
+import subprocess
+from PIL import Image
+import random
+from element import *
+def clef_range(clef):
+    if clef=="treble":
+        fix_octave = str(random.choice(["c'","c''"]))
+    elif clef == "alto" or clef== "tenor":
+        fix_octave = str(random.choice(["c","c'"]))
+    elif clef=="bass":
+        fix_octave = str(random.choice(["c,","c"]))
+    return fix_octave
+    
+def score_generation(level):
+    while True:  # This loop will keep running until a valid answer is found
+        clef1 = random.choice(["treble", "bass"])
+        clef2 = random.choice(["treble", "bass"])
+        if level == 0:
+            clef2 = clef1
+            fix_octave1 = clef_range(clef1)
+            fix_octave2 = fix_octave1
+        else:
+            if level ==1:
+                pass
+            elif level ==2:
+                if clef1 == clef2:
+                    continue
+            elif level ==3:
+                clef1 = random.choice(["tenor","alto"])
+                clef2 = random.choice(["tenor","alto"])
+            elif level >=4:
+                clef1 = random.choice(["treble", "bass","tenor","alto"])
+                clef2 = random.choice(["treble", "bass","tenor","alto"])
+                
+            fix_octave1 = clef_range(clef1)
+            fix_octave2 = clef_range(clef2)
+
+        if level == 0:
+            accidentals = [basic_accidentals[1]]
+        elif level >= 1 and level<=4:
+            accidentals = basic_accidentals
+        elif level >= 5:
+            accidentals = advance_accidentals
+
+        output_note = [random.choice(note_letters), random.choice(accidentals), fix_octave1]
+        output_note2 = [random.choice(note_letters), random.choice(accidentals), fix_octave2]
+
+        if output_note==output_note2 and random.randint(0, 3) != 0:
+            continue
+
+        note = f"{output_note[0]}{accidentals_lilypond[output_note[1]]}".lower()
+        note2 = f"{output_note2[0]}{accidentals_lilypond[output_note2[1]]}".lower()
+
+        interval_number = calculate_interval(fix_octave1, fix_octave2, note_letters.index(output_note[0]), note_letters.index(output_note2[0]))
+        semitone_count = calculate_semitone(note, note2, note_letters.index(output_note[0]), note_letters.index(output_note2[0]),fix_octave1, fix_octave2)
+        print(interval_number,semitone_count)
+        if interval_number >= 24:
+            interval_number -= 2
+        elif interval_number > 15 and interval_number <=23:
+            interval_number -= 1 
+        key = (str(interval_number%7), str(semitone_count))
+        # Check if the key exists in JUMP_CHART
+        if key in JUMP_CHART:
+            e_ans = JUMP_CHART[key]
+            no_ans = NUM_PLACEMENT[str(interval_number)]
+            ans = f"{e_ans} {no_ans}"
+            print(note, note2, ans)
+            return ans,clef1,clef2,fix_octave1, fix_octave2, note, note2
+
+def lilypond_generation(clef, clef2,fix_octave1,fix_octave2,note,note2):
+    lilypond_score = f"""
+  \\version "2.24.3"  
+  \\header {{}}
+    tagline = "" 
+  
+  #(set-global-staff-size 26) 
+
+  \\score {{
+    {{
+      \\clef "{clef}" 
+      \\fixed {fix_octave1} {note}
+      \\clef "{clef2}"
+      \\fixed {fix_octave2} {note2}
+    }}
+    \\layout {{
+      indent = 0\\mm  % Remove indentation to avoid unnecessary space
+      line-width = #50  % Adjust line width to fit your content
+      ragged-right = ##f  % To avoid ragged right lines
+      \\context {{
+        \\Score
+        \\omit TimeSignature
+        \\remove "Bar_number_engraver"  % Remove bar numbers
+      }}
+    }}
+  }}
+  """
+
+  # Write the string to a LilyPond (.ly) file
+    with open('scorew.ly', 'w') as f:
+      f.write(lilypond_score)
+
+  # Run LilyPond on the file to generate the score
+  # Ensure that LilyPond is in your system's PATH or provide the full path to the LilyPond executable
+    subprocess.run(['lilypond', '--png', '-dresolution=300', 'scorew.ly'], check=False)
+
+  # Open the generated PNG file
+    with Image.open('scorew.png') as img:
+      # Calculate the crop rectangle
+      width, height = img.size
+      crop_height = height
+      crop_rectangle = (120, 0, width/4, crop_height/10)
+      
+      # add code to crop 1/10 from the top
+      cropped_img = img.crop(crop_rectangle)
+      cropped_img.save("static/images/cropped_score_ans.png")
+    
+    
+
+
+"""clef = random.choice(["treble", "bass"])
+        clef2 = random.choice(["treble", "bass"])
+        if lv == 0:
+            clef2 = clef
+            fix_octave1 = clef_range(clef)
+            fix_octave2 = fix_octave1
+        elif lv ==1:
+            fix_octave1 = clef_range(clef)
+            fix_octave2 = clef_range(clef2)
+            pass
+        elif lv ==2:
+            if clef == clef2:
+                continue
+            fix_octave1 = clef_range(clef)
+            fix_octave2 = clef_range(clef2)
+        elif lv ==3:
+            clef = random.choice(["treble", "bass","tenor","alto"])
+            clef2 = random.choice(["treble", "bass","tenor","alto"])
+            fix_octave1 = clef_range(clef)
+            fix_octave2 = clef_range(clef2)
+
+        """
---
+++ interval_quiz.py
@@ -0,0 +1,66 @@
+import streamlit as st
+from generation import *
+from element import user_quality, user_interval
+
+def main():
+    st.set_page_config(page_title="Music Note App")
+    if 'new_quest' not in st.session_state:
+        st.session_state['new_quest'] = True
+    if 'pic' not in st.session_state:
+        st.session_state['pic'] = False
+    
+    def new_question():
+        difficulty = st.session_state.get('difficulty', 0)
+        ans, clef, clef2, fix_octave1, fix_octave2, note, note2 = score_generation(level=difficulty)
+        lilypond_generation(clef, clef2, fix_octave1, fix_octave2, note, note2)
+        st.session_state['current_answer'] = ans
+        st.session_state['feedback'] = ""
+        st.session_state['pic'] = True
+
+    def check_answer():
+        user_ans = f"{st.session_state['user_quality']} {st.session_state['user_interval']}"
+        current_answer = st.session_state.get('current_answer')
+
+        if current_answer is None:
+            feedback = 'Error: No current answer available. Please try a new question.'
+        elif user_ans.lower() == current_answer.lower():
+            feedback = 'Correct!'
+            st.session_state['new_quest'] = True
+        else:
+            feedback = f'Incorrect. The answer should be {current_answer}'
+
+        st.session_state['feedback'] = feedback
+    
+    st.title("Music Note App")
+
+    difficulty = st.selectbox("Choose difficulty:", [
+        "Beginner", "Intermediate", "Advanced", "C clef Fanfare", "Expert", "Accidental Madness"
+    ], index=st.session_state.get('difficulty', 0))
+    st.session_state['difficulty'] = ["Beginner", "Intermediate", "Advanced", "C clef Fanfare", "Expert", "Accidental Madness"].index(difficulty)
+
+    col1, col2 = st.columns(2)
+    if st.session_state['pic']==True:
+        image_path = f"/Users/chakhangchan/Documents/VS_code/Music_theory_app/interval/flask_wclef/static/images/cropped_score_ans.png"
+        st.image(image_path, use_column_width=True)
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Qualityans=st.selectbox("Quality", user_quality, key='user_quality')
+
+    with col2:
+        Intervalans=st.selectbox("Interval", user_interval, key='user_interval')
+
+    with col3:
+        if st.button("Check Answer") and Qualityans != "--"and Intervalans!= "--":
+            check_answer()
+            if st.session_state.get('feedback'):
+                st.write(st.session_state['feedback'])
+
+    if st.button("New Question"):
+        st.session_state['pic']=False
+        new_question()
+        st.rerun()
+
+if __name__ == '__main__':
+    main()
